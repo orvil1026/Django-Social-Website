cd3182494ca3981a2248ab9bcd894bf1c9569139
Orvil D'silva <orvildsilva@gmail.com>
2023-08-10 11:37:40 +0530

    home html page

---
+++ homepage/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+from django.contrib.auth.decorators import login_required
+from django.views.i18n import JavaScriptCatalog
+
+
+app_name = "homepage"
+
+urlpatterns = [
+
+    path('page/', views.home, name='home')
+
+    ]
