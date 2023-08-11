f9515176c2add7012c79667665011e5bc39c7555
Orvil D'silva <orvildsilva@gmail.com>
2023-08-11 11:09:26 +0530

    buy credit page added

--- homepage/views.py
+++ homepage/views.py
@@ -9,3 +9,5 @@ def nav(request):
     return render(request, 'nav.html')
 def profile(request):
     return render(request, 'profile.html')
+def buy_credits(request):
+    return render(request, 'buy_credits.html')
